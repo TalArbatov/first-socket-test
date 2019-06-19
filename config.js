6c6c20d1f85eaac83a67df7160bb5eba5d9e3eca
Tal Arbatov <talarbatov98@gmail.com>
2019-06-19 15:19:36 +0300

    changed socket uri config to be compateble with heroku

--- config.js
+++ config.js
@@ -4,4 +4,5 @@ module.exports = {
     isProduction: process.env.NODE_ENV === 'production' ? true : false,
     port: process.env.PORT || 3000,
     mongoURI: process.env.MONGO_URI,
+    socketURI: process.env.SOCKET_URI
 }
